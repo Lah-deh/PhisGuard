44f9e15f4b9b4b1334bd55d3d7d6348ff048fdd1
lah-deh <succssasokere@gmail.com>
2025-06-29 08:13:22 -1100

    worked on responsiveness

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className='hero'>
         <div className='heroo'>
           <h1>Stay Safe Online with PhisGuard</h1>
-          <p>Detect Phishing links instantly and protect yourself from malicious with one click.</p>
+          <p>Detect Phishing links instantly and protect yourself from malicious websites with one click.</p>
         </div>
         <button>Install Extension</button>
       </div>
