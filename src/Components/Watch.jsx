a3f3158b5cfc03642fe8fb2360f5c0786546e77f
lah-deh <succssasokere@gmail.com>
2025-07-08 22:03:12 -1100

    fixed the layout

--- src/Components/Watch.jsx
+++ src/Components/Watch.jsx
@@ -5,10 +5,17 @@ import React from 'react'
 const Watch = () => {
   return (
     <div className='Watch'>
-      <h3>Watch PhisGuard in Action</h3>
-      <video controls className='demo'>
-        <source src='/video.mp4' type='video/mp4'/>
-      </video>
+      <h3>✅How to install PhisGuard</h3>
+      <ul>
+        <li>Click the <span>Install Extension</span> button.</li>
+        <li>Extract (unzip) the downloaded file.</li>
+        <li>Open chrome://extensions in your chrome broswer.</li>
+        <li>Turn on <span>Developer mode</span> (top right toggle).</li>
+        <li>Click <span>Load unpacked</span>.</li>
+        <li>Select the unzipped PhisGuard folder.</li>
+        <li>done! The extension will appear in your toolbar.</li>
+      </ul>
+      
     </div>
   )
 }
