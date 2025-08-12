76d7a50c109cdd7f0e87e582de1ae6d87d6aa636
lah-deh <succssasokere@gmail.com>
2025-08-12 12:19:37 -1100

    fixed the api its calling

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -11,7 +11,7 @@ const Feed = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://phishguard-api-0nyx.onrender.com', {
+      const response = await fetch('https://phishguard-api-0nyx.onrender.com/predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ url: link })
